363aba83a512d17b60a9699bbdc39007eae1f381
Imad Arrada <imad-seddik.arrada@capgemini.com>
2022-02-08 00:14:16 +0100

    Fixing typos in Login component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -12,7 +12,7 @@ function Login() {
         let result = await login(email, password); // Utilisation de la fonction login
     
         if (!result) {
-            throw new Error(`Error while login : ${result}`)
+            throw new Error(`Error while login : ${result}`);
         }
 
         console.log(result);
@@ -34,7 +34,7 @@ function Login() {
                     <TextField value={password} onChange={(e) => setPassword(e.target.value)} id="password" label="Password" type="password" autoComplete="current-password"/>
                 </div>
                 <div className="mt-6 flex flex-col justify-center">
-                    <Button variant="text" onClick={handleLogin}>Sign up</Button>
+                    <Button variant="text" onClick={handleLogin}>Sign in</Button>
                     <p className="text-sm text-center">Don't have an account yet? <span className="underline font-bold"><Link to="/register">Sign up</Link></span></p>
                 </div>
             </div>
