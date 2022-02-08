bf2e08cc2b96bdf7be1ff8eb2ffdcc2117dbb293
Andréa Ricciutelli <andrea.ricciutelli@imac-de-andrea.home>
2022-02-08 09:29:11 +0100

    Commit du Postform

--- src/Components/Postform.js
+++ src/Components/Postform.js
@@ -1,11 +1,52 @@
 import React, {useState} from "react";
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { createPost } from "../lib/social-network-library"; // Import de la fonction login
+
 
 function Postform () {
-    return (
-        <div className="">
-            <p> Hohoho</p>
-        </div>
 
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const handleCreatePost = async () => {
+        let result = await createPost(title, content); // Utilisation de la fonction login
+       
+        if (!result) {
+            throw new Error(`Error while registering : ${result}`)
+        }
+
+        console.log(result);
+    }
+
+
+
+    return (
+        <div className="grow flex flex-col justify-center items-center">
+            <div className="max-w-lg w-full shadow-md rounded-lg p-8">
+                <div className="flex flex-col space-y-4">
+                    <Typography variant="h2" align="center" gutterBottom component="div">
+                    A quoi <br/> pensez-vous ?
+                    </Typography>
+                    <TextField 
+                        id="posttitle" 
+                        label="Title" 
+                        variant="outlined" 
+                        value={title} onChange={(e) => setTitle(e.target.value)} />
+
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Content"
+                        multiline
+                        rows={4}
+                        defaultValue=""
+                        value={content} onChange={(e) => setContent(e.target.value)}
+                    />
+                    <Button onClick={handleCreatePost} variant="contained">Publier</Button>
+                </div>
+            </div>
+        </div>
 
     )
 
